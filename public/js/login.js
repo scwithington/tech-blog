7108673b5fa7735eb9d21bf38221b5ea09fbd052
Cal Withington <scwithington@gmail.com>
2021-04-27 19:02:03 -0400

    login js and handlebars

---
+++ public/js/login.js
@@ -0,0 +1,50 @@
+const loginFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#username').value.trim();
+    const password = document.querySelector('#password').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Login failed.');
+      }
+    }
+  };
+  
+  const signupFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const username = document.querySelector('#userSignup').value.trim();
+    const password = document.querySelector('#passSignup').value.trim();
+  
+    if (username && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ username, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert('Sign-up failed.');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.loginForm')
+    .addEventListener('submit', loginFormHandler);
+  
+  document
+    .querySelector('.signupForm')
+    .addEventListener('submit', signupFormHandler);
+  
